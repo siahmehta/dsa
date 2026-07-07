601bda10cc7bb4e252204f7af977061881f38e1b
siahmehta <2024ucp1245@mnit.ac.in>
2026-07-07 23:17:35 +0530

    Time: 0 ms (100%), Space: 8.4 MB (39.01%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int getnext(int n){
+        int sum=0;
+        while(n>0){
+            int digit=n%10;
+            sum+=digit*digit;
+            n=n/10;
+        }
+        return sum;
+    }
+    bool isHappy(int n) {
+        unordered_set <int> seen;
+        while(n!=1 && !seen.count(n)){
+            seen.insert(n);
+            n=getnext(n);
+        }
+        return n==1;
+    }
+};
