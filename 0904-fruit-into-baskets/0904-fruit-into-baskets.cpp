41b27e6729b445b65ea937ee551b295165ebe147
siahmehta <2024ucp1245@mnit.ac.in>
2026-07-09 09:12:35 +0530

    Time: 40 ms (68.62%), Space: 84.8 MB (83.7%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int,int> freq;
-        int left=0;
-        int ans=0;
-        for(int right=0; right<fruits.size();right++){
+       unordered_map<int,int> freq;
+       int ans=0;
+       int left=0;
+       for(int right=0; right<fruits.size(); right++){
             freq[fruits[right]]++;
-            while(freq.size()>2){
+            if(freq.size()>2){
                 freq[fruits[left]]--;
                 if(freq[fruits[left]]==0){
                     freq.erase(fruits[left]);
+                    
                 }
                 left++;
             }
-            ans=max(ans,right-left+1);
-        }
-        return ans;
+           ans=max(ans,right-left+1);  
+       }
+      return ans;
     }
 };
